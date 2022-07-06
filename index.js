85a83b31f31dbb570246a61b78ed91d48d5ddf78
code kazuki <agbowurotoyin8@gmail.com>
2022-07-06 20:51:50 +0100

    fellow

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+let count = 0
+document.getElementById('count-el').innerText=count
+
+function increment() {
+    count = count + 1
+    document.getElementById('count-el').innerText=count
+
+}
+
+
+
+
+function decrement() {
+    count = count - 1
+    document.getElementById('count-el').innerText=count
+
+}
+function reset() {
+    count = 0
+    document.getElementById('count-el').innerText=count
+
+}
+
